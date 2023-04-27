0631d4d7fed72744cbf8b5c0daf48a491d9a2f45
Brian <mahuibrian14@gmail.com>
2023-04-27 11:44:04 +0300

    complete

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -10,6 +10,27 @@ function QuestionForm(props) {
     correctIndex: 0,
   });
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    });
+  }
+
   function handleChange(event) {
     setFormData({
       ...formData,
@@ -17,10 +38,6 @@ function QuestionForm(props) {
     });
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-  }
 
   return (
     <section>
